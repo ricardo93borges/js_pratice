6b3fd5511901997bc145ffec9f5dd599f1e5c821
Ricardo Borges <ricardo93borges@gmail.com>
2018-06-03 20:36:40 -0300

    first commit

---
+++ src/map.js
@@ -0,0 +1,16 @@
+'use strict'
+
+const map = (arr = [], func) => {
+    if(typeof func !== 'function') {
+        throw new TypeError('func is not a function')
+    }
+
+    let newArr = []
+    for( let i=0; i < arr.length; i++){
+        newArr.push(func(arr[i], i, arr))
+    }
+    return newArr
+
+}
+
+export default map
